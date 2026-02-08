6c2d401a64b5b90a7eb532be196bf26438a27871
Karthik5412 <kkuricharlapati@gmail.com>
2026-02-08 15:14:44 +0530

    Done

--- app.py
+++ app.py
@@ -1,30 +1,53 @@
 import streamlit as st
 import pandas as pd
 import joblib
+from streamlit_extras.let_it_rain import rain
 
-model = joblib.load('knn_model.plk')
+model = joblib.load('lg_model.plk')
 scale = joblib.load('scale.plk')
+
+st.set_page_config(page_title='Penguin Classification', page_icon= '🐧', layout= 'wide')
+
+page_bg = '''
+    <style>
+        [data-testid="stAppViewContainer"] {
+        background: linear-gradient(180deg, #090909 0%, #271033 50%, #1e3c72 100%);
+        background-attachment: fixed;
+        background-position: center;
+}
+        }
+    </style>
+'''
+st.markdown(page_bg, unsafe_allow_html=True)
 
 st.title('PENGUIN CLASSIFIER')
 
 col1, col2, col3, col4 = st.columns(4)
 
 with col1 :
+    
+    st.subheader('Culmen Length')
     c_len = st.number_input('',30,60,43)
     
 with col2 :
+    
+    st.subheader('Culmen Depth')
     c_dpt = st.number_input('',10,25,17)
     
 with col3 :
+    
+    st.subheader('Flipper Length')
     f_len = st.slider('',170,235,201)
     
 with col4 :
+    
+    st.subheader('Body Mass')
     mass = st.slider('',2600,6400,4202)
 
 btn = st.button('Find')
 
 if btn :
-    st.balloons()
+    st.snow()
     
     input_row = {
         'culmen_length_mm' : [c_len],
@@ -52,8 +75,16 @@ if btn :
             island = 'Dream'
         case 2 :
             island = 'Torgersen'
-            
+    
     st.success(f'It is a {species} penguin. This is from {island} island')
+    rain(
+        emoji="🐧",
+        font_size=75,
+        falling_speed= 5,
+        animation_length= 'short'
+    )
+    
+    
     
     
     
